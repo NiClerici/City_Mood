d976e3598c3d18bcc1171b0acadb2898eb58ba5d
Emilie Jaquemet <emilie.jaquemet@stud.hslu.ch>
2026-04-16 14:30:32 +0200

    BugFixx

--- init-metabase.sql
+++ init-metabase.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS metabase CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS 'metabase_user'@'%' IDENTIFIED BY 'metabase_password';
+CREATE USER IF NOT EXISTS 'metabase_user'@'%' IDENTIFIED WITH mysql_native_password BY 'metabase_password';
 GRANT ALL PRIVILEGES ON metabase.* TO 'metabase_user'@'%';
 FLUSH PRIVILEGES;
