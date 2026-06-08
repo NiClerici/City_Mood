8b054baab38bc430953ec16f7b4af621cd7e30e8
NiClerici <nico.clerici@stud.hslu.ch>
2026-06-08 12:19:53 +0200

    Refactor mood score calculation procedure

--- calculate_mood_scores.sql
+++ calculate_mood_scores.sql
@@ -1,29 +1,54 @@
--- Stored Procedure: Calculates mood scores from raw data
-
 USE mood_city;
 DELIMITER //
 
 CREATE PROCEDURE IF NOT EXISTS calculate_mood_scores(
-    IN p_city_id INT,
+    IN p_city_id   INT,
     IN p_timestamp DATETIME
 )
 BEGIN
-    INSERT INTO MOOD_SCORE (city_id, timestamp, weather_score, sbb_score, traffic_score, mood_score)
-    SELECT 
-        p_city_id, p_timestamp,
-        GREATEST(0, LEAST(1, 1 - (w.weathercode / 100.0))),
-        GREATEST(0, LEAST(1, 1 - (s.delay_minutes * 3 / 300.0))),
-        GREATEST(0, LEAST(1, t.current_speed / NULLIF(t.free_flow_speed, 0))),
-        GREATEST(0, LEAST(1,
-            0.4 * GREATEST(0, LEAST(1, 1 - (w.weathercode / 100.0))) +
-            0.3 * GREATEST(0, LEAST(1, 1 - (s.delay_minutes * 3 / 300.0))) +
-            0.3 * GREATEST(0, LEAST(1, t.current_speed / NULLIF(t.free_flow_speed, 0)))
-        ))
-    FROM WEATHER w
-    JOIN SBB s USING (city_id, timestamp)
-    JOIN TRAFFIC t USING (city_id, timestamp)
-    WHERE w.city_id = p_city_id AND w.timestamp = p_timestamp
-    LIMIT 1;
+    -- Lokale Variablen fuer die berechneten Scores
+    DECLARE v_weather_score  FLOAT DEFAULT 0;
+    DECLARE v_sbb_score      FLOAT DEFAULT 0;
+    DECLARE v_traffic_score  FLOAT DEFAULT 0;
+    DECLARE v_mood_score     FLOAT DEFAULT 0;
+    DECLARE v_count          INT   DEFAULT 0;
+
+    -- Pruefen ob Rohdaten fuer Stadt + Zeitpunkt vorhanden sind
+    SELECT COUNT(*) INTO v_count
+    FROM   WEATHER w
+    JOIN   SBB     s USING (city_id, timestamp)
+    JOIN   TRAFFIC t USING (city_id, timestamp)
+    WHERE  w.city_id   = p_city_id
+      AND  w.timestamp = p_timestamp;
+
+    -- Nur berechnen wenn alle drei Datenquellen vorhanden sind
+    IF v_count > 0 THEN
+
+        SELECT
+            GREATEST(0, LEAST(1, 1 - (w.weathercode / 100.0))),
+            GREATEST(0, LEAST(1, 1 - (s.delay_minutes * 3 / 300.0))),
+            GREATEST(0, LEAST(1, COALESCE(t.current_speed / NULLIF(t.free_flow_speed, 0), 0)))
+        INTO v_weather_score, v_sbb_score, v_traffic_score
+        FROM   WEATHER w
+        JOIN   SBB     s USING (city_id, timestamp)
+        JOIN   TRAFFIC t USING (city_id, timestamp)
+        WHERE  w.city_id   = p_city_id
+          AND  w.timestamp = p_timestamp
+        LIMIT  1;
+
+        -- Gewichteter Mood-Score
+        SET v_mood_score = GREATEST(0, LEAST(1,
+            0.4 * v_weather_score +
+            0.3 * v_sbb_score     +
+            0.3 * v_traffic_score
+        ));
+
+        -- Berechnete Scores speichern
+        INSERT INTO MOOD_SCORE (city_id, timestamp, weather_score, sbb_score, traffic_score, mood_score)
+        VALUES (p_city_id, p_timestamp, v_weather_score, v_sbb_score, v_traffic_score, v_mood_score);
+
+    END IF;
+
 END//
 
 DELIMITER ;
